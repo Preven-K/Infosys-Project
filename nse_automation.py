2a2b5385e153abf5af8939971ed3a9e055c36db8
PREVEN K <117811244+Preven-K@users.noreply.github.com>
2025-02-17 23:53:09 +0530

    Update nse_automation.py

--- nse_automation.py
+++ nse_automation.py
@@ -2,8 +2,6 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
